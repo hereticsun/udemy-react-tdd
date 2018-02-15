fdc4914da39f4b8438a78505312b48b518e2e996
Azlan Cuttilan <azlan@cuttilan.co.uk>
2018-02-15 12:34:04 +0000

    Support redux deposit and withdraw methods

--- lootcheck/src/actions/balance.js
+++ lootcheck/src/actions/balance.js
@@ -3,6 +3,20 @@ import * as constants from './constants';
 export const setBalance = balance => {
   return {
     type: constants.SET_BALANCE,
-    balance
+    balance,
+  }
+};
+
+export const deposit = deposit => {
+  return {
+    type: constants.DEPOSIT,
+    deposit,
+  }
+};
+
+export const withdraw = withdrawal => {
+  return {
+    type: constants.WITHDRAW,
+    withdrawal,
   }
 };
--- lootcheck/src/actions/balance.test.js
+++ lootcheck/src/actions/balance.test.js
@@ -10,3 +10,23 @@ it('creates an action to set a balance', () => {
 
   expect(actions.setBalance(balance)).toEqual(expectedAction);
 });
+
+it('creates an action to deposit into the balance', () => {
+  const deposit = 10;
+  const expectedAction = { 
+    type: constants.DEPOSIT,
+    deposit,
+  };
+
+  expect(actions.deposit(deposit)).toEqual(expectedAction);
+});
+
+it('creates an action to withdraw from the balance', () => {
+  const withdrawal = 10;
+  const expectedAction = {
+    type: constants.WITHDRAW,
+    withdrawal,
+  };
+
+  expect(actions.withdraw(withdrawal)).toEqual(expectedAction);
+});
